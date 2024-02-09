25bcc18a056cd641e85e42cc973b24b6a22af503
Sirio2022 <105917953+Sirio2022@users.noreply.github.com>
2024-02-09 13:40:10 -0500

    first commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,24 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div>
+      <header>
+        TS Ecommerce
+      </header>
+
+      <main>
+
+      </main>
+
+      <footer>
+        All rights reserved &copy; &#128640; {new Date().getFullYear()}
+      </footer>
+    </div>
+  )
+}
+
+export default App
