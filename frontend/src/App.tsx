42f0efb14966ad98bf9bff3d28c29517325117d4
Sirio2022 <105917953+Sirio2022@users.noreply.github.com>
2024-02-10 09:00:57 -0500

    Bootstrap

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,33 +1,50 @@
 
-import './App.css'
+
 import { formatoMoneda } from './utils/Utils'
 import { sampleProducts } from './data'
+import { Col, Container, Nav, Navbar, Row } from 'react-bootstrap'
 
 function App() {
 
 
   return (
-    <div>
+    <div className='d-flex flex-column vh-100'>
       <header>
-        TS Ecommerce
+        <Navbar bg='dark' variant='dark' expand='lg' >
+
+          <Container>
+            <Navbar.Brand href='/'>Music Store</Navbar.Brand>
+          </Container>
+
+          <Nav>
+            <Nav.Link href='/cart'>Cart</Nav.Link>
+            <Nav.Link href='/signin'>Sign In</Nav.Link>
+          </Nav>
+
+        </Navbar>
       </header>
 
       <main>
-        <ul>
-          {sampleProducts.map((product) => (
-            <li
-              key={product.slug}
-            >
-              <img src={product.image} alt={product.name} className='product-image' />
-              <h2>{product.name}</h2>
-              <p>{formatoMoneda(product.price)}</p>
-            </li>
-          ))}
-        </ul>
+        <Container className='mt-3'>
+          <Row>
+            {sampleProducts.map((product) => (
+              <Col
+                key={product.slug}
+              >
+                <img src={product.image} alt={product.name} className='product-image' />
+                <h2>{product.name}</h2>
+                <p>{formatoMoneda(product.price)}</p>
+              </Col>
+            ))}
+          </Row>
+        </Container>
       </main>
 
       <footer>
-        All rights reserved &copy; Juan Manuel Alvarez &#128640; {new Date().getFullYear()}
+        <div className='text-center'>
+
+          All rights reserved &copy; Juan Manuel Alvarez &#128640; {new Date().getFullYear()}
+        </div>
       </footer>
     </div>
   )
