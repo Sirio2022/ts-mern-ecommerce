86e5763e7f33b111399d48b1bfc2775f24f2b8dc
Sirio2022 <105917953+Sirio2022@users.noreply.github.com>
2024-02-12 06:53:42 -0500

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { Col, Container, Nav, Navbar, Row } from 'react-bootstrap'
+import { Container, Nav, Navbar } from 'react-bootstrap'
 import { Outlet } from 'react-router-dom'
 
 function App() {
